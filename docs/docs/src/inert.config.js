3026b0ec7bb72a3e165226a9afffeb4c73ebd6de
Jake <pygamer138@gmail.com>
2020-11-19 20:23:56 +0100

    changes to documentation website

--- docs/docs/src/inert.config.js
+++ docs/docs/src/inert.config.js
@@ -32,6 +32,40 @@ const config = {
     ],
     plugins: ['plugin.js'],
     assets: './assets',
+    sidebar: [{
+        type: 'heading',
+        text: 'Getting Started'
+    }, {
+        type: 'link',
+        href: '/docs/introduction',
+        text: 'Introduction'
+    }, {
+        type: 'link',
+        href: '/docs/getting_started',
+        text: 'Your first eon.js server'
+    }, {
+        type: 'heading',
+        text: 'Advanced'
+    }, {
+        type: 'link',
+        href: '/docs/advanced',
+        text: 'Advanced Examples'
+    }, {
+        type: 'link',
+        href: '/docs/debugging',
+        text: 'Debugging'
+    }, {
+        type: 'heading',
+        text: 'API'
+    }, {
+        type: 'link',
+        href: '/docs/api',
+        text: 'Core API'
+    }, {
+        type: 'link',
+        href: '/docs/plugins',
+        text: 'Plugins API'
+    }]
 }
 
 module.exports = config;
